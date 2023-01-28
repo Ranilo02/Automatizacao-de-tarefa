7e1ca951e4595a56149909f2ccdd1fe5082d8868
Ranilo <ranilocabral@gmail.com>
2023-01-28 11:16:15 -0300

    Update Renovação de livros.py

--- Renovação de livros.py
+++ Renovação de livros.py
@@ -1,59 +1,42 @@
-import pyautogui            #biblioteca de métodos para controlar mouse e teclado
-from time import sleep      #mecanismo de controle de tempo
+import pyautogui
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 
-# esse comando separa cada passo do programa em 1 segundo
-pyautogui.PAUSE = 1
 
-# esperar 3 segundos
+#Guardar login e senha de acesso
+login = input('Login do SIGAA: ')
+senha = input('Senha do SIGAA: ')
+
+
+#iniciar o navegador, acessar o site e faz login no sistema SIGAA
+navegador = webdriver.Chrome()
+navegador.get('https://sigaa.ufpi.br/sigaa/verTelaLogin.do;jsessionid=D425BF5D3957F6BA33A8E9418617AE15.jb05')
+navegador.find_element('xpath', '//*[@id="conteudo"]/div[3]/form/table[1]/tbody/tr[1]/td/input').send_keys(login, Keys.TAB, senha, Keys.ENTER)
+
+
+#encontrar o caminho para a opção de renovação de empréstimos navegando no menu suspenso
+menu_suspenso = navegador.find_element('xpath', '//*[@id="menu_form_menu_discente_j_id_jsp_1325243614_85_menu"]/table/tbody/tr/td[6]/span[2]')
+acoes = ActionChains(navegador)
+acoes.move_to_element(menu_suspenso).perform()
+
+menu_suspenso = navegador.find_element('xpath', "//*[text() = 'Empréstimos']")
+acoes = ActionChains(navegador)
+acoes.move_to_element(menu_suspenso).perform()
+
+menu_suspenso = navegador.find_element('xpath', "//*[text() = 'Renovar Meus Empréstimos']")
+acoes = ActionChains(navegador)
+acoes.move_to_element(menu_suspenso).perform()
+menu_suspenso.click()
+
+#clica no botão de selecionar todos, dá uma pausa de 3 segundos e confirma
+renovar_todos = navegador.find_element('xpath', '//*[@id="formularioRenovamaMeusEmprestimos"]/table/thead/tr/th[1]/input')
+renovar_todos.click()
 sleep(3)
+renovar_todos = navegador.find_element('xpath', '//*[@id="formularioRenovamaMeusEmprestimos:cmdButtonConfirmarRenovacaoMeusEmprestimos"]')
+renovar_todos.click(),sleep(3)
+pyautogui.press('ENTER')
 
-#apertar a tecla windows para abrir o menu iniciar
-pyautogui.press('win')
-
-#escrever o nome do programa a ser buscado no menu iniciar
-pyautogui.write('brave')
-sleep(3)
-
-
-pyautogui.press('enter')
-sleep(5)
-
-#pyautogui.click(x=887, y=251)         #uso esse comando no caso de o navegador não abrir em tela cheia
-
-# escrever endereço do site da universidade
-pyautogui.write('https://sigaa.ufpi.br/sigaa')
-pyautogui.press('enter')
-sleep(5)
-
-# aqui você digita seu login e em seguida a senha
-pyautogui.write('*login*')
-pyautogui.press('tab')
-pyautogui.write('*senha*')
-pyautogui.press('enter')
-
-
-sleep(5)
-pyautogui.press('tab')
-#sleep(0.3)
-pyautogui.press('tab')
-#sleep(0.3)
-pyautogui.press('tab')
-#sleep(1)
-pyautogui.press('enter')
-sleep(4)
-
-# Sequência de clicks do mouse em posições previamente estabelecidas
-pyautogui.click(x=631, y=159)
-pyautogui.click(x=861, y=247)
-pyautogui.click(x=1028, y=281)
-sleep(2.5)
-pyautogui.click(x=201, y=351)
-pyautogui.click(x=657, y=508)
-pyautogui.press('enter')
-
-
-#código para descobrir a posição do mouse em que efetuará o click
-# sleep(5)
-# n = pyautogui.position()
-# print(n)
+sleep(15)
 
